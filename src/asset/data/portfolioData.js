9f97ebd21829f3b595b1e3f912fa66a968424f64
semiherd <smherdogan@gmail.com>
2023-06-09 21:02:25 +0300

    update repo

--- src/asset/data/portfolioData.js
+++ src/asset/data/portfolioData.js
@@ -1,8 +1,22 @@
 import project1 from '../image/project1.png';
 import project2 from '../image/project2.png';
 import project3 from '../image/project3.png';
+import project4 from '../image/project4.png';
+import project5 from '../image/project5.png';
 
 const portfolioData=[
+	{
+		title: 'Animation on Scroll - ReactJs',
+		url: 'https://semiherd.github.io/onscrollanimation/',
+		image: project4,
+		portrait: false
+	},
+	{
+		title: 'Responsive Flight Search SPA - ReactJs',
+		url: 'https://semiherd.github.io/eurowings_spa_reactjs /',
+		image: project5,
+		portrait: false
+	},
 	{
 		title: 'Mobile First Design - ReactJs',
 		url: 'https://semiherd.github.io/mobilefirstdesign_digitalmarketing/',
