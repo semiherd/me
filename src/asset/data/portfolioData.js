2065097f7cd8795d173aaa66303761d51b7fa271
semiherd <smherdogan@gmail.com>
2023-06-09 21:04:32 +0300

    update repo

--- src/asset/data/portfolioData.js
+++ src/asset/data/portfolioData.js
@@ -13,7 +13,7 @@ const portfolioData=[
 	},
 	{
 		title: 'Responsive Flight Search SPA - ReactJs',
-		url: 'https://semiherd.github.io/eurowings_spa_reactjs /',
+		url: 'https://semiherd.github.io/eurowings_spa_reactjs/',
 		image: project5,
 		portrait: false
 	},
