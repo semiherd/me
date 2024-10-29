76774fd314433c872917bb433b6e0995d45511cd
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2024-10-29 16:21:17 +0300

    Update socialData.js

--- src/asset/data/socialData.js
+++ src/asset/data/socialData.js
@@ -2,27 +2,45 @@ import twitter from '../image/twitter.png';
 import instagram from '../image/instagram.png';
 import linkedin from '../image/linkedin.png';
 import github from '../image/github.png';
+import youtube from '../image/youtube.png';
+import xing from '../image/xing.png';
 
 const socialData=[
 	{
-		id:'linkedin',
+		id:'LinkedIn',
 		image: linkedin,
+		color: '#0a66c2',
 		url:'https://www.linkedin.com/in/semih-erdogan'
 	},
 	{
-		id:'github',
-		image: github,
-		url:'https://github.com/semiherd'
+		id:'Xing',
+		image: xing,
+		color: '#0698A0',
+		url:'https://www.xing.com/profile/Semih_Erdogan5'
 	},
 	{
-		id:'instagram',
-		image:instagram,
+		id:'Github',
+		image: github,
+		color: '#2b3137',
+		url:'https://github.com/semiherd',
+	},
+	{
+		id:'Instagram',
+		image: instagram,
+		color: '#405DE6',
 		url:'https://instagram.com/semiherdgan'
 	},
 	{
-		id:'twitter',
-		image:twitter,
+		id:'Twitter',
+		image: twitter,
+		color: '#1DA1F2',
 		url:'https://twitter.com/semiherdogan'
 	},
+	{
+		id:'Youtube',
+		image: youtube,
+		color: '#ff0000',
+		url:'https://www.youtube.com/@semiherdogan1032'
+	}
 ]
 export default socialData;
