0d14a33870da5574bc21071d335a5babcf186760
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2024-10-29 16:20:59 +0300

    Update portfolioData.js

--- src/asset/data/portfolioData.js
+++ src/asset/data/portfolioData.js
@@ -1,46 +1,66 @@
 import hoc_searchbar from '../image/hoc_searchbar.png';
-import project2 from '../image/project2.png';
 import project3 from '../image/project3.png';
 import project4 from '../image/project4.png';
 import project5 from '../image/project5.png';
 import project8 from '../image/project8.png';
+import portfoliovideo1 from '../video/portfolio1.mov';
+import portfoliovideo2 from '../video/portfolio2.mov';
+import portfoliovideo3 from '../video/portfolio3.mov';
 
-const portfolioData=[
+const portfolioVideo=[
+	{
+		title: '3d-Animation with Three.Js',
+		url: 'https://github.com/semiherd/3d-book-threejs',
+		source: 'https://drive.google.com/file/d/1fVOHtZ6TTH2EwPuOM48G5bgBo9IyAd2_/view?usp=drive_link',
+		portrait: false
+	},
+	{
+		title: 'Hexagon Shaped Gallery',
+		url: 'https://github.com/semiherd/hexagon-gallery-css',
+		source: 'https://drive.google.com/file/d/1g90YwuunhyovD50UxmxKG_WiPcCGtYhI/view?usp=drive_link',
+		portrait: false
+	},
+	{
+		title: 'Scrollable Header - React Native',
+		url: 'https://github.com/semiherd/scrollableheader',
+		source: 'https://drive.google.com/file/d/1g90YwuunhyovD50UxmxKG_WiPcCGtYhI/view?usp=drive_link',
+		portrait: true
+	}
+]
+
+const portfolioImage=[
 	{
 		title: 'Animation on Scroll - ReactJs',
 		url: 'https://semiherd.github.io/onscrollanimation/',
-		image: project4,
+		source: project4,
 		portrait: false
 	},
 	{
 		title: 'Responsive Flight Search SPA - ReactJs',
 		url: 'https://github.com/semiherd/eurowings_spa_reactjs/',
-		image: project5,
+		source: project5,
 		portrait: false
 	},
 	{
 		title: 'Animated Sidebar - ReactJs',
 		url: 'https://github.com/semiherd/reactjs_sidebar',
-		image: project8,
+		source: project8,
 		portrait: false
 	},
 	{
 		title: 'Search Bar HOC - ReactJs & TS',
 		url: 'https://codesandbox.io/s/hoc-search-bar-auto-complete-5h4cld',
-		image: hoc_searchbar,
+		source: hoc_searchbar,
 		portrait: false
-	},
-	{
-		title: 'Guidein App - ReactNative',
-		url: 'https://apps.apple.com/tr/app/guidein/id1664757527',
-		image: project2,
-		portrait: true
 	},
 	{
 		title: 'Auth Screen - ReactNative',
 		url: 'https://github.com/semiherd/rn_authscreen',
-		image: project3,
+		source: project3,
 		portrait: true
 	},
 ]
-export default portfolioData;
+export {
+	portfolioVideo,
+	portfolioImage
+}
