d6e30c629a923d02c9b7bb32aed727a8a35298a1
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2024-10-30 14:01:52 +0300

    Update Portfolio.js

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -1,22 +1,21 @@
-import dosthgreat from '../asset/image/dosthgreat.jpg';
 import { portfolioImage, portfolioVideo } from '../asset/data/portfolioData';
 
 const PortfolioItem= ({data,type}) => {
 	return (
 		<div className="project-item">
-			<div className={data.portrait==true? 'image-portrait':'image-landscape'} >	
-				<a 
-					href={data.url} 
-					rel="noopener noreferrer" 
-					target="_blank" >{data.title}	
-					{type==='image'
-						? <img src={data.source} alt={data.title} />
-						: type==='video'
-							? <video src={data.source} width={`75%`} height={`75%`} controls></video>
-							: null
-					}
-				</a>
-			</div>
+			<h1>{data.title}</h1>
+			<a 
+				href={data.url} 
+				rel="noopener noreferrer" 
+				target="_blank" >
+				{type==='image'
+					? <img src={data.source} width={data.portrait ?`35%` :`90%`} height={data.portrait ?`90%` :`80%`} alt={data.title} />
+					: type==='video'
+						? <iframe class="responsive-iframe" src={data.source} width={data.portrait ?`25%` :`75%`} height={data.portrait ?`100%` :`100%`}></iframe>
+						//? <video src={`https://drive.google.com/uc?export=view&id=1fVOHtZ6TTH2EwPuOM48G5bgBo9IyAd2_`} width={`75%`} height={`75%`} controls></video>
+						: null
+				}
+			</a>
 		</div>
 	)
 }
