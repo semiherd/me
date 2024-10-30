d83a42db79bdb15f8bf10b53d17b83ef5b0481bf
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2024-10-30 14:02:14 +0300

    Update portfolioData.js

--- src/asset/data/portfolioData.js
+++ src/asset/data/portfolioData.js
@@ -8,24 +8,36 @@ const portfolioVideo=[
 	{
 		title: '3d-Animation with Three.Js',
 		url: 'https://github.com/semiherd/3d-book-threejs',
-		source: 'https://drive.google.com/file/d/1fVOHtZ6TTH2EwPuOM48G5bgBo9IyAd2_/view?usp=drive_link',
+		source: 'https://drive.google.com/file/d/1fVOHtZ6TTH2EwPuOM48G5bgBo9IyAd2_/preview',
 		portrait: false
 	},
 	{
 		title: 'Hexagon Shaped Gallery',
 		url: 'https://github.com/semiherd/hexagon-gallery-css',
-		source: 'https://drive.google.com/file/d/1g90YwuunhyovD50UxmxKG_WiPcCGtYhI/view?usp=drive_link',
+		source: 'https://drive.google.com/file/d/17E5AKiE9Kh_OSA4ua0gZ2u06APNF0BYE/preview',
 		portrait: false
 	},
 	{
 		title: 'Scrollable Header - React Native',
 		url: 'https://github.com/semiherd/scrollableheader',
-		source: 'https://drive.google.com/file/d/1g90YwuunhyovD50UxmxKG_WiPcCGtYhI/view?usp=drive_link',
+		source: 'https://drive.google.com/file/d/1g90YwuunhyovD50UxmxKG_WiPcCGtYhI/preview',
 		portrait: true
 	}
 ]
 
 const portfolioImage=[
+	{
+		title: 'Auth Screen - ReactNative',
+		url: 'https://github.com/semiherd/rn_authscreen',
+		source: project3,
+		portrait: true
+	},
+	{
+		title: 'Animated Sidebar - ReactJs',
+		url: 'https://github.com/semiherd/reactjs_sidebar',
+		source: project8,
+		portrait: false
+	},
 	{
 		title: 'Animation on Scroll - ReactJs',
 		url: 'https://semiherd.github.io/onscrollanimation/',
@@ -39,23 +51,12 @@ const portfolioImage=[
 		portrait: false
 	},
 	{
-		title: 'Animated Sidebar - ReactJs',
-		url: 'https://github.com/semiherd/reactjs_sidebar',
-		source: project8,
-		portrait: false
-	},
-	{
 		title: 'Search Bar HOC - ReactJs & TS',
 		url: 'https://codesandbox.io/s/hoc-search-bar-auto-complete-5h4cld',
 		source: hoc_searchbar,
 		portrait: false
 	},
-	{
-		title: 'Auth Screen - ReactNative',
-		url: 'https://github.com/semiherd/rn_authscreen',
-		source: project3,
-		portrait: true
-	},
+	
 ]
 export {
 	portfolioVideo,
