e94dab513e3e490838f57b6030b704b2c7910e13
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2025-05-29 13:20:00 +0300

    Update portfolio.js

--- components/portfolio/portfolio.js
+++ components/portfolio/portfolio.js
@@ -1,6 +1,28 @@
 const containerPortfolio= document.querySelector('section.portfolio');
 containerPortfolio.style= "display: grid;";
 
+function createOptions(data){
+    const optionsList= createDOMElement({ type: 'div', attributes:{ class: 'tabs'}});
+    const optionElm= (option) => createDOMElement({type: 'li', attributes: { class: 'tab active'},text: option });
+    data.forEach( t => {
+        const newOption= optionElm(t.label);
+        optionsList.append(newOption);
+    });
+    return { techOptions: optionsList };
+}
+function createTechFilterBar(techList){
+    const barContainer= createDOMElement({
+        type: 'div',
+        attributes: { id: 'portfolio-scrollbar', class: "scrollbar" }
+    });
+    const { lefticon, righticon } = createArrows();
+    const { techOptions } = createOptions(techList);
+    barContainer.append(lefticon);  
+    barContainer.append(techOptions);
+    barContainer.append(righticon);
+    return barContainer;
+}
+
 function renderFilterScrollbar(){
     const filterBar= createTechFilterBar(techIcons);
     filterByTechHandler(filterBar);
