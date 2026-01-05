2f831e518cebfc79163af1e0631dbd47020c2403
Semih Erdogan <semiherdogan@192.168.1.46>
2026-01-05 13:29:44 +0300

    update baseurl

--- js/urls.js
+++ js/urls.js
@@ -1,14 +1,15 @@
+const base= import.meta.env.BASEURL
 export const images= {
-    semihPhoto: "/images/semih.jpeg",
-    angularWebsite: "/images/angular-website.png",
-    productionReporting: "/images/production-reporting.png",
-    threejsBooklet: "/images/threejs-booklet.png",
-    threeJsLanding: "/images/three-js-landing.png",
-    awsCdk: "/images/aws_cdk.webp",
-    azure: "/images/azure.png"
+    semihPhoto: `${base}/images/semih.jpeg`,
+    angularWebsite: `${base}/images/angular-website.png`,
+    productionReporting: `${base}/images/production-reporting.png`,
+    threejsBooklet: `${base}/images/threejs-booklet.png`,
+    threeJsLanding: `${base}/images/three-js-landing.png`,
+    awsCdk: `${base}/images/aws_cdk.webp`,
+    azure: `${base}/images/azure.png`
 }
 export const urls= {
     fonts: {
-        ItallianoRegular: "/fonts/Italianno/Italianno-Regular.ttf"  
+        ItallianoRegular: `${base}/fonts/Italianno/Italianno-Regular.ttf` 
     }
 }
