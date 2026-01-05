a58c4129dbf4c73c489a04f00fb00832133f3c67
Semih Erdogan <semiherdogan@192.168.1.46>
2026-01-05 14:41:04 +0300

    update profile image url

--- js/urls.js
+++ js/urls.js
@@ -1,11 +1,10 @@
 
 export const images= {
-    semihPhoto: "../images/semih.jpeg",
-    angularWebsite: "../images/angular-website.png",
-    productionReporting: "../images/production-reporting.png",
-    threejsBooklet: "../images/threejs-booklet.png",
-    threeJsLanding: "../images/three-js-landing.png",
-    awsCdk: "../images/aws_cdk.webp",
+    angularWebsite: "./images/angular-website.png",
+    productionReporting: "./images/production-reporting.png",
+    threejsBooklet: "./images/threejs-booklet.png",
+    threeJsLanding: "./images/three-js-landing.png",
+    awsCdk: "./images/aws_cdk.webp",
     azure: "./images/azure.png"
 }
 export const urls= {
