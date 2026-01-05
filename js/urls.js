590935007bf486fc6601c6b2ab994e765fa28de5
Semih Erdogan <semiherdogan@192.168.1.46>
2026-01-05 14:23:36 +0300

    update urls

--- js/urls.js
+++ js/urls.js
@@ -1,15 +1,15 @@
 
 export const images= {
-    semihPhoto: "/images/semih.jpeg",
-    angularWebsite: "/images/angular-website.png",
-    productionReporting: "/images/production-reporting.png",
-    threejsBooklet: "/images/threejs-booklet.png",
-    threeJsLanding: "/images/three-js-landing.png",
-    awsCdk: "/images/aws_cdk.webp",
-    azure: "/images/azure.png"
+    semihPhoto: "./images/semih.jpeg",
+    angularWebsite: "./images/angular-website.png",
+    productionReporting: "./images/production-reporting.png",
+    threejsBooklet: "./images/threejs-booklet.png",
+    threeJsLanding: "./images/three-js-landing.png",
+    awsCdk: "./images/aws_cdk.webp",
+    azure: "./images/azure.png"
 }
 export const urls= {
     fonts: {
-        ItallianoRegular: "/fonts/Italianno/Italianno-Regular.ttf"  
+        ItallianoRegular: "./fonts/Italianno/Italianno-Regular.ttf"  
     }
 }
